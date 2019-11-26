ca65cdc2dfdb1dbab84618caa24bccd6821f5cc5
Alessandro Oshiro <alessandro.oshiro@usp.br>
2019-11-26 01:01:01 +0100

    Updates

---
+++ precache-manifest.fd5abeab9030303f6548631b397933fd.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4d4a81c21e0ffa65fec4b5d5b060902e",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "44a1c27a5bae71e5aaa5",
+    "url": "/portfolio/static/css/2.5bc09813.chunk.css"
+  },
+  {
+    "revision": "1800ebfef99ac036282a",
+    "url": "/portfolio/static/css/main.7bf3daf8.chunk.css"
+  },
+  {
+    "revision": "44a1c27a5bae71e5aaa5",
+    "url": "/portfolio/static/js/2.e995d7f2.chunk.js"
+  },
+  {
+    "revision": "1800ebfef99ac036282a",
+    "url": "/portfolio/static/js/main.fa23cf5c.chunk.js"
+  },
+  {
+    "revision": "76aa8cbd67b5c937ec2c",
+    "url": "/portfolio/static/js/runtime-main.e92c632a.js"
+  },
+  {
+    "revision": "82208ce71066ffa9f501e0350e6e72bc",
+    "url": "/portfolio/static/media/IBF1.82208ce7.png"
+  },
+  {
+    "revision": "c989d4853832ce6cdba8e533debbeb57",
+    "url": "/portfolio/static/media/IBF2.c989d485.png"
+  },
+  {
+    "revision": "861a1cd6572e435105aee09444aad3ff",
+    "url": "/portfolio/static/media/Real2u-1.861a1cd6.png"
+  },
+  {
+    "revision": "611f9f688ce299f7e7b616163abe593a",
+    "url": "/portfolio/static/media/Real2u-2.611f9f68.png"
+  },
+  {
+    "revision": "333d954ee802416617556c598524941c",
+    "url": "/portfolio/static/media/Renato1.333d954e.png"
+  },
+  {
+    "revision": "ba6c72915f96a04a8a32829c325213d4",
+    "url": "/portfolio/static/media/Renato2.ba6c7291.png"
+  },
+  {
+    "revision": "395432dd1fc5df9dc9e02d945d0be220",
+    "url": "/portfolio/static/media/fau1.395432dd.png"
+  },
+  {
+    "revision": "fb296608c8c38eb4657c8f15c1ea4d47",
+    "url": "/portfolio/static/media/fau2.fb296608.png"
+  },
+  {
+    "revision": "3791e70a90ddea96de1bbdc3afeab7a5",
+    "url": "/portfolio/static/media/furniture1.3791e70a.png"
+  }
+]);
