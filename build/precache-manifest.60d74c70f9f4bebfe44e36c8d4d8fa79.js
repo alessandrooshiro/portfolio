e7ba5321e2b792e0c9492a477d5cd5d17047dd2d
Alessandro Oshiro <alessandro.oshiro@usp.br>
2019-12-26 23:37:02 +0000

    deploy

---
+++ build/precache-manifest.60d74c70f9f4bebfe44e36c8d4d8fa79.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a8001cce2a34ec1602007a3044356d2d",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "248d174e694c2a467730",
+    "url": "/portfolio/static/css/2.5bc09813.chunk.css"
+  },
+  {
+    "revision": "9c030d07c2984830c116",
+    "url": "/portfolio/static/css/main.3da43e1d.chunk.css"
+  },
+  {
+    "revision": "248d174e694c2a467730",
+    "url": "/portfolio/static/js/2.66467e3e.chunk.js"
+  },
+  {
+    "revision": "9c030d07c2984830c116",
+    "url": "/portfolio/static/js/main.3b5e5892.chunk.js"
+  },
+  {
+    "revision": "76aa8cbd67b5c937ec2c",
+    "url": "/portfolio/static/js/runtime-main.e92c632a.js"
+  },
+  {
+    "revision": "82208ce71066ffa9f501e0350e6e72bc",
+    "url": "/portfolio/static/media/IBF1.82208ce7.png"
+  },
+  {
+    "revision": "c989d4853832ce6cdba8e533debbeb57",
+    "url": "/portfolio/static/media/IBF2.c989d485.png"
+  },
+  {
+    "revision": "861a1cd6572e435105aee09444aad3ff",
+    "url": "/portfolio/static/media/Real2u-1.861a1cd6.png"
+  },
+  {
+    "revision": "611f9f688ce299f7e7b616163abe593a",
+    "url": "/portfolio/static/media/Real2u-2.611f9f68.png"
+  },
+  {
+    "revision": "333d954ee802416617556c598524941c",
+    "url": "/portfolio/static/media/Renato1.333d954e.png"
+  },
+  {
+    "revision": "ba6c72915f96a04a8a32829c325213d4",
+    "url": "/portfolio/static/media/Renato2.ba6c7291.png"
+  },
+  {
+    "revision": "395432dd1fc5df9dc9e02d945d0be220",
+    "url": "/portfolio/static/media/fau1.395432dd.png"
+  },
+  {
+    "revision": "fb296608c8c38eb4657c8f15c1ea4d47",
+    "url": "/portfolio/static/media/fau2.fb296608.png"
+  },
+  {
+    "revision": "56743e8f10d2568ec3759797790fbe0b",
+    "url": "/portfolio/static/media/profile-pic.56743e8f.jpg"
+  },
+  {
+    "revision": "ae1a984cced57d5201ef0b0fde46701a",
+    "url": "/portfolio/static/media/textures1.ae1a984c.png"
+  },
+  {
+    "revision": "961eb962bae169425a172dc33c5646fe",
+    "url": "/portfolio/static/media/textures2.961eb962.png"
+  }
+]);
