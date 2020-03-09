922d88fd97548ff86ff4c5d4f92319643ece5a8e
Alessandro Oshiro <alessandro.oshiro@usp.br>
2020-03-08 22:05:28 -0300

    update images

--- src/components/projects/item.js
+++ src/components/projects/item.js
@@ -17,36 +17,31 @@ class Item extends Component {
                     onMouseLeave={this.props.onHover}
                     onClick={this.props.openProject}
                 >
-
-                    {/* <Fade bottom big>
-                        <div className={styles.detailsContainer}> */}
-                            <div
-                                className={this.props.isProjectOpen == true ? styles.projectTitleExtended : styles.projectTitle}
-                            >
-                                <Fade bottom>
-                                    {this.props.project.name}
-                                </Fade>
-                                <Fade bottom>
-                                    <button className={styles.linkButton}>
-                                        <a href={this.props.project.link} target="_blank" rel="noopener noreferrer">
-                                            {
-                                                this.props.project.link
-                                                    ? (<img src={linkIcon} className={styles.linkIcon} alt='link icon' />)
-                                                    : (null)
-                                            }
-                                        </a>
-                                    </button>
-                                </Fade>
-                            </div>
-                            <div
-                                className={this.props.isProjectOpen == true ? styles.projectDescriptionExtended : styles.projectDescription}
-                            >
-                                <Fade bottom>
-                                    {this.props.project.smallDescription}
-                                </Fade>
-                            </div>
-                        {/* </div>
-                    </Fade> */}
+                    <div
+                        className={this.props.isProjectOpen == true ? styles.projectTitleExtended : styles.projectTitle}
+                    >
+                        <Fade bottom>
+                            {this.props.project.name}
+                        </Fade>
+                        <Fade bottom>
+                            <button className={styles.linkButton}>
+                                <a href={this.props.project.link} target="_blank" rel="noopener noreferrer">
+                                    {
+                                        this.props.project.link
+                                            ? (<img src={linkIcon} className={styles.linkIcon} alt='link icon' />)
+                                            : (null)
+                                    }
+                                </a>
+                            </button>
+                        </Fade>
+                    </div>
+                    <div
+                        className={this.props.isProjectOpen == true ? styles.projectDescriptionExtended : styles.projectDescription}
+                    >
+                        <Fade bottom>
+                            {this.props.project.smallDescription}
+                        </Fade>
+                    </div>
                     <div
                         className={this.props.isProjectOpen == true ? styles.imageContainerExtended : styles.imageContainer} >
                         <Fade bottom big>
@@ -65,14 +60,14 @@ class Item extends Component {
                                     ? (
                                         <div>
                                             <img
-                                                src={this.props.project.photoB}
+                                                src={this.props.project.photoC}
                                                 className={styles.projectImage3}
-                                                alt='project B'
+                                                alt='project C'
                                             />
                                             <img
-                                                src={this.props.project.photoA}
+                                                src={this.props.project.photoD}
                                                 className={styles.projectImage4}
-                                                alt='project A'
+                                                alt='project D'
                                             />
                                         </div>
                                     ) : (
--- src/components/projects/projects.js
+++ src/components/projects/projects.js
@@ -1,13 +1,23 @@
 import Real2uA from '../../images/Real2u-1.png'
 import Real2uB from '../../images/Real2u-2.png'
+import Real2uC from '../../images/Real2u-3.png'
+import Real2uD from '../../images/Real2u-4.png'
 import RenatoA from '../../images/Renato1.png'
 import RenatoB from '../../images/Renato2.png'
+import RenatoC from '../../images/Renato3.png'
+import RenatoD from '../../images/Renato4.png'
 import FauA from '../../images/fau1.png'
 import FauB from '../../images/fau2.png'
+import FauC from '../../images/fau3.png'
+import FauD from '../../images/fau4.png'
 import TexturesA from '../../images/textures1.png'
 import TexturesB from '../../images/textures2.png'
+import TexturesC from '../../images/textures3.png'
+import TexturesD from '../../images/textures4.png'
 import IBFA from '../../images/IBF1.png'
 import IBFB from '../../images/IBF2.png'
+import IBFC from '../../images/IBF3.png'
+import IBFD from '../../images/IBF4.png'
 
 
 const projects = [
@@ -16,35 +26,45 @@ const projects = [
     smallDescription: "Renato Cobra developed the Cobra Method, focused in increasing the student's performance. I participated in the development of his website as front-end developer and desginer.",
     link: 'https://www.renatocobra.com.br/',
     photoA: RenatoA,
-    photoB: RenatoB
+    photoB: RenatoB,
+    photoC: RenatoC,
+    photoD: RenatoD
   },
   {
     name: 'OpenTextures.io',
     smallDescription: "OpenTextures.io is an online single page application to help tech artists find the perfect 3D Texture for their models. I was responsible for the design and front-end development of the project.",
     link: 'http://www.opentextures.io',
     photoA: TexturesA,
-    photoB: TexturesB
+    photoB: TexturesB,
+    photoC: TexturesC,
+    photoD: TexturesD
   },
   {
     name: 'Real2U',
     smallDescription: "Website project for  Real2U, an augmented reality focused startup. I was in charge of creating both the company's website and a dashboard for clients and admin use.",
     link: 'https://www.real2u.com.br',
     photoA: Real2uA,
-    photoB: Real2uB
+    photoB: Real2uB,
+    photoC: Real2uC,
+    photoD: Real2uD
   },
   {
     name: 'IBF Project',
     smallDescription: "Website for the Internet Balancing Formula Project, consisting of a few introduction pages and a dynamic form. My tasks included both the design and front-end development of the entire website.",
     link: 'http://www.internetbalancingformula.com/',
     photoA: IBFA,
-    photoB: IBFB
+    photoB: IBFB,
+    photoC: IBFC,
+    photoD: IBFD
   },
   {
     name: 'FauUsp',
     smallDescription: "Website redesign project for the Faculty of Architecture and Urbanism of the University of São Paulo (FAUUSP). Not published.",
     link: null,
     photoA: FauA,
-    photoB: FauB
+    photoB: FauB,
+    photoC: FauC,
+    photoD: FauD
   }
 ]
 
