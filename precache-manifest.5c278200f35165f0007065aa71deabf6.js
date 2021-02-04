ea8bf6205477d177e2ae04ee045e77b449c8305d
Alessandro Oshiro <alessandro.oshiro@usp.br>
2021-02-03 22:01:39 -0300

    Updates

---
+++ precache-manifest.5c278200f35165f0007065aa71deabf6.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "99f6dad98c41a37299f6c067dfe16538",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "20f43da6aa67de828133",
+    "url": "/portfolio/static/css/main.22436c3d.chunk.css"
+  },
+  {
+    "revision": "c20f7d2c23a0e4c9779f",
+    "url": "/portfolio/static/js/2.255cd8aa.chunk.js"
+  },
+  {
+    "revision": "20f43da6aa67de828133",
+    "url": "/portfolio/static/js/main.547e655b.chunk.js"
+  },
+  {
+    "revision": "751306707218722ac78e",
+    "url": "/portfolio/static/js/runtime-main.6394bb44.js"
+  },
+  {
+    "revision": "26aa96eed49fcf945200556b78e7ad7f",
+    "url": "/portfolio/static/media/brush-stroke-banner-6.26aa96ee.svg"
+  },
+  {
+    "revision": "26fe5acb02d31db088a7d3d845580a7b",
+    "url": "/portfolio/static/media/logo@2x.26fe5acb.png"
+  },
+  {
+    "revision": "e3b2d5fb70ac8596c927bf3a0be9e499",
+    "url": "/portfolio/static/media/real2u1@2x.e3b2d5fb.png"
+  },
+  {
+    "revision": "fbdf498f9c5e46a8c5302e9186f7229b",
+    "url": "/portfolio/static/media/real2u2.fbdf498f.png"
+  },
+  {
+    "revision": "1ebfef680143aeeff5be7e2c4b0cfe77",
+    "url": "/portfolio/static/media/real2u3.1ebfef68.png"
+  },
+  {
+    "revision": "f3e58881e4953f563418e39f78098285",
+    "url": "/portfolio/static/media/renderfarm1@2x.f3e58881.png"
+  },
+  {
+    "revision": "26eeae4d45e77331fcbe4b9bc0737fae",
+    "url": "/portfolio/static/media/renderfarm2@2x.26eeae4d.png"
+  },
+  {
+    "revision": "92bd5ba9b3ba3d61b59f7e36f3d01e82",
+    "url": "/portfolio/static/media/renderfarm3@2x.92bd5ba9.png"
+  },
+  {
+    "revision": "88c4b39683ac43eb07c743cc436b3285",
+    "url": "/portfolio/static/media/reviewer2u.88c4b396.png"
+  },
+  {
+    "revision": "10f967c66b44fffc889b5f2afa6ecea8",
+    "url": "/portfolio/static/media/reviewer2u2.10f967c6.png"
+  },
+  {
+    "revision": "189a995759803f49161f0873f637980f",
+    "url": "/portfolio/static/media/reviewer2u3.189a9957.png"
+  },
+  {
+    "revision": "54196f72247cd6fa9f24fc157e608b56",
+    "url": "/portfolio/static/media/reviewer2u4.54196f72.png"
+  },
+  {
+    "revision": "ffb31d122cf805bc68b349d953f7a95c",
+    "url": "/portfolio/static/media/reviewer2u5.ffb31d12.png"
+  },
+  {
+    "revision": "16a650d6b3242465ffc556a170fb90c4",
+    "url": "/portfolio/static/media/reviewer2u6.16a650d6.png"
+  },
+  {
+    "revision": "6c207c75771d11bda1d7a0ba76969ed1",
+    "url": "/portfolio/static/media/reviewer2u7.6c207c75.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.2e106797fea83f207610513aff3627f6.js"
+  "/portfolio/precache-manifest.5c278200f35165f0007065aa71deabf6.js"
 );
 
 self.addEventListener('message', (event) => {
